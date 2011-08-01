887e8e94fcd9d5322e889ce7b97fe741f74ffe12
Johannes Finnsson <johannes.finnsson@gmail.com>
2011-08-01 17:34:29 +0200

    initial - redis connected

---
+++ app.js
@@ -0,0 +1,112 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    formidable = require('formidable'),
+    exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
+    url = require('url'),
+    fs = require('fs'),
+    requestHandlers = require("./requestHandlers"),
+    redis = require('redis'),
+    redisClient = redis.createClient();
+
+ 
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Data Store
+
+redisClient.on("error", function(err) {
+  console.log("Error " + err);
+});
+
+ var case1 = {
+    "title": "case1",
+    "radios": [123,123]};
+ redisClient.set("case:1:page:1", JSON.stringify(case1));
+
+
+// redisClient.set("string key", "string val", redis.print);
+// redisClient.incr("numberOfCases", redis.print);
+// redisClient.hset("hash key", "hashtest 1", "some value", redis.print);
+// redisClient.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+// redisClient.hkeys("hash key", function (err, replies) {
+//   console.log(replies.length + " replies:");
+//   replies.forEach(function (reply, i) {
+//     console.log("     " + i + ": " + reply);
+//  });
+//  redisClient.quit();
+// });
+
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+app.get('/case/:id/:page', function(req, res){
+  var findCase="case:"+req.params.id+":page:"+req.params.page;
+  redisClient.get(findCase, function( err, data) {
+  var theCase = JSON.parse( data.toString() );
+  res.render('case', {
+      title: theCase.title,
+      scripts: ['jquery.min.js', 'jquery.mousewheel.min.js', 'stacks.js'],
+      radios: theCase.radios, 
+    });
+  });
+});
+
+app.get('/image/:id', function(req, res){
+  var image = __dirname + '/img/' + req.params.id + '.jpg';
+  fs.readFile(image, "binary", function(error, file){
+    if(error) {
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'image/jpeg');
+      res.write(file, "binary");
+      res.end();
+    }
+  });
+});
+
+app.get('/upload', function(req, res){
+  res.render('upload', {
+    imageid: '123', 
+    title: 'upload'
+  });
+});
+
+app.post('/image/:id', function(req, res){
+  requestHandlers.postImage(req, res);
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ requestHandlers.js
@@ -0,0 +1,58 @@
+var formidable = require('formidable'),
+    exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
+    url = require('url'),
+    fs = require('fs');
+
+function postImage(req, res){
+  var d = req.params.id;
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(files.upload.type == 'application/zip'){
+      fs.mkdir(d,0755, function (){
+      var child1 = exec('unzip -d ' + d + ' ' + files.upload.path, // unzips the upload
+      function (error, stdout, stderr) {
+//        console.log('stdout: ' + stdout);
+//        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+        var child2 = exec('find ' + d + ' -type f -exec mv {} ' + d + ' \\;', 
+        function (error, stdout, stderr) {
+//          console.log('stdout: ' + stdout);
+//          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          var child3 = exec('gm identify ./' + d + '/*.jpg',
+          function (error, stdout, stderr) {
+//          console.log('stdout: ' + stdout);
+//          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+            var child4 = exec('gm montage -geometry 512 -tile 100000x1 ./' + d + '/*.jpg ' + __dirname + '/public/images/' + d + '.jpg', // makes a montage of uploaded images
+            function (error, stdout, stderr) {
+//            console.log('stdout: ' + stdout);
+//            console.log('stderr: ' + stderr);
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+            var child5 = exec('rm -r ' +  d, // removes temporary directory
+            function (error, stdout, stderr) {
+//              console.log('stdout: ' + stdout);
+//              console.log('stderr: ' + stderr);
+              if (error !== null) {
+                console.log('exec error: ' + error);
+              }
+            });
+           });
+         });
+        });
+       });
+      });
+    }
+  });
+}
+
+exports.postImage = postImage;
