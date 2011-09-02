fb85d0e157212097b6c2fb5dd679e10f02c5d161
Johannes Finnsson <johannes.finnsson@gmail.com>
2011-09-03 00:30:48 +0200

    annoying parenthesis

--- keys_file.js
+++ keys_file.js
@@ -8,6 +8,6 @@ module.exports = {
   twitter: {
     consumerkey: 'wnzFhTkWJbjVLKuGuR08g',
     consumersecret: 'dAzpAuPkOKqsxiBFpqVapyME5klC3x3ARBgUnoz2A',
-    callback: 'http://localhost:3000/auth/twitter_callback'
+    callback: 'http://178.79.158.252:3000/auth/twitter_callback'
   }
 }
