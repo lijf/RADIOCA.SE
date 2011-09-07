d77df2e616e3c600db09a48371dc986261c1ec49
Johannes Finnsson <johannes.finnsson@gmail.com>
2011-09-07 22:47:03 +0200

    working twitter oauth

--- keys_file.js
+++ keys_file.js
@@ -8,6 +8,6 @@ module.exports = {
   twitter: {
     consumerkey: 'wnzFhTkWJbjVLKuGuR08g',
     consumersecret: 'dAzpAuPkOKqsxiBFpqVapyME5klC3x3ARBgUnoz2A',
-    callback: 'http://178.79.158.252:3000/auth/twitter_callback'
+    callback: 'http://local.host:3000/auth/twitter_callback'
   }
 }
