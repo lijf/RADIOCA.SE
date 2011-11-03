e0da714e96b1a9fd5d96038274feb434ba2a7a49
Johannes Finnsson <johannes.finnsson@gmail.com>
2011-11-03 10:29:56 +0100

    cleaning client.js

--- app.js
+++ app.js
@@ -25,307 +25,298 @@ var app = module.exports = express.createServer(
 // Configuration
 
 app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'eventuallycloseduringnative' }));
-    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-    app.use(easyoauth(require('./keys_file')));
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'eventuallycloseduringnative' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(easyoauth(require('./keys_file')));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function() {
-    // app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  // app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function() {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 });
 
 app.error(function(err, req, res, next) {
-    sys.puts('APP.ERROR:' + sys.inspect(err));
-    next(err);
+  sys.puts('APP.ERROR:' + sys.inspect(err));
+  next(err);
 });
 
 
 // Data Store
 
 db.on('error', function(err) {
-    console.log('Redis Error ' + err);
+  console.log('Redis Error ' + err);
 });
 
 function include(arr, obj) {
-    return (arr.indexOf(obj) != -1);
+  return (arr.indexOf(obj) != -1);
 }
 // Helper functions
 
 function username(req, res) {
-    if (req.isAuthenticated())return req.getAuthDetails().user.username;
-    else return '0'
+  return req.isAuthenticated() ? req.getAuthDetails().user.username : '0';
 }
 
 // Routes
 
 app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Home',
-        signed_in: req.isAuthenticated(),
-        user: username(req, res)
-    });
+  res.render('index', {
+    title: 'Home',
+    signed_in: req.isAuthenticated(),
+    user: username(req, res)
+  });
 });
 
 app.get('/newcase', function(req, res) {
-    if (req.isAuthenticated()) {
-        res.render('newcase', {
-            title: 'Create new case',
-            signed_in: req.isAuthenticated(),
-            user: req.getAuthDetails().user.username
-        });
-    }
-    else res.redirect('/')
+  if (!req.isAuthenticated()) res.redirect('/');
+  else res.render('newcase', {
+    title: 'Create new case',
+    signed_in: req.isAuthenticated(),
+    user: req.getAuthDetails().user.username
+  });
 });
 
 app.post('/newcase', function(req, res) {
-    if (req.isAuthenticated()) {
-        var data = req.body;
-        data.creator = req.getAuthDetails().user.username;
-        //console.log(data);
-        data.texts = ['Double click to add text'];
-        var day = new Date();
-        var cid = day.getTime().toString();
-        data.cid = cid;
-        if (!data.meta_private) db.lpush('cases', cid);
-        else db.sadd('private', cid);
-        db.sadd('cases:' + data.creator, cid);
-        db.hmset('case:' + cid + ':page:1', data,
-            function(err) {
-                db.sadd('case:' + cid + ':users', req.getAuthDetails().user.user_id,
-                    function() {
-                        console.log('created case: ' + cid);
-                        res.send('/case/' + cid + '/1', 200);
-                    });
-            });
-    }
-    else {
-        res.send('FORBIDDEN', 403)
-    }
+  if (!req.isAuthenticated()) res.send('FORBIDDEN', 403);
+  else {
+    var data = req.body;
+    data.creator = req.getAuthDetails().user.username;
+    //console.log(data);
+    data.texts = ['Double click to add text'];
+    var day = new Date();
+    var cid = day.getTime().toString();
+    data.cid = cid;
+    if (!data.meta_private) db.lpush('cases', cid);
+    else db.sadd('private', cid);
+    db.sadd('cases:' + data.creator, cid);
+    db.hmset('case:' + cid + ':page:1', data, function(err) {
+      db.sadd('case:' + cid + ':users', req.getAuthDetails().user.user_id, function() {
+        console.log('created case: ' + cid);
+        res.send('/case/' + cid + '/1', 200);
+      });
+    });
+  }
 });
 
 function getCases(req, res, data, i, sendcases, db) {
-    if (data[i]) {
-        db.hgetall('case:' + data[i] + ':page:1', function(err, sendcase) {
-            sendcases[i] = sendcase;
-            i++;
-            if (data[i]) {
-                getCases(data, i, sendcases)
-            }
-            else {
-                console.log('rendering cases');
-                res.render('cases', {
-                    title: 'Cases',
-                    signed_in: req.isAuthenticated(),
-                    user: req.getAuthDetails().user.username,
-                    cases: sendcases
-                })
-            }
-        });
-    }
-    else {
-        return 'No data'
-    }
+  if (!data[i]) return 'No data';
+  else {
+    db.hgetall('case:' + data[i] + ':page:1', function(err, sendcase) {
+      sendcases[i] = sendcase;
+      i++;
+      if (data[i]) {
+        getCases(data, i, sendcases)
+      }
+      else {
+        console.log('rendering cases');
+        res.render('cases', {
+          title: 'Cases',
+          signed_in: req.isAuthenticated(),
+          user: req.getAuthDetails().user.username,
+          cases: sendcases
+        })
+      }
+    });
+  }
 }
 
 app.get('/cases/:start/:finish', function(req, res) {
-    if (req.isAuthenticated()) {
-        var start = parseInt(req.params.start, 10);
-        var end = parseInt(req.params.finish, 10);
-        db.lrange('cases', start, end, function(err, data) {
-            if (err || !data[0]) res.send('404', 404);
-            var i = 0;
-            var sendcases = [];
-            requestHandlers.getCases(req, res, data, i, sendcases, db);
+  if (!req.isAuthenticated()) res.redirect('/');
+  else {
+    var start = parseInt(req.params.start, 10);
+    var end = parseInt(req.params.finish, 10);
+    db.lrange('cases', start, end, function(err, cases) {
+      if (err || !cases[0]) res.send('404', 404);
+      var sendcases = [];
+      cases.forEach(function(theCase, iteration) {
+        db.hgetall('case:' + theCase + ':page:1', function(err, sendcase) {
+          sendcases[iteration] = sendcase;
+          if (!cases[iteration + 1]) {
+            console.log('rendering cases');
+            res.render('cases', {
+              title: 'Cases',
+              signed_in: req.isAuthenticated(),
+              user: req.getAuthDetails().user.username,
+              cases: sendcases
+            });
+          }
         });
-    }
-    else res.redirect('/');
+      });
+    });
+  }
 });
 
 app.get('/case/:id/:page', function(req, res) {
-    if (req.isAuthenticated()) {
-        db.sismember(
-            'case:' + req.params.id + ':users',
-            req.getAuthDetails().user.user_id,
-            function(err, editor) {
-                db.hgetall("case:" + req.params.id + ":page:" + req.params.page,
-                    function(err, theCase) {
-                        if (err || !theCase.cid) res.redirect('back');
-                        if (!theCase.meta_private || (theCase.meta_private && editor)) {
-                            requestHandlers.rendercase(req, res, theCase, editor, db);
-                        }
-                        res.redirect('back');
-                    });
-            });
-    }
-    else res.redirect('/');
+  if (!req.isAuthenticated()) res.redirect('/');
+  else {
+    db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id, function(err, editor) {
+      db.hgetall("case:" + req.params.id + ":page:" + req.params.page, function(err, theCase) {
+        if (err || !theCase.cid) res.redirect('back');
+        if (!theCase.meta_private || (theCase.meta_private && editor)) {
+          requestHandlers.rendercase(req, res, theCase, editor, db);
+        }
+        res.redirect('back');
+      });
+    });
+  }
 });
 
 app.get('/signed_in', function(req, res) {
-    var uid = req.getAuthDetails().user.user_id;
-    var userdata = JSON.stringify(req.getAuthDetails());
-    //console.dir(req.getAuthDetails());
-    db.sismember('users', uid, function(err, data) {
-        if (!data) {
-            db.sismember('invitees', req.getAuthDetails().user.username, function(err, data) {
-                if (!data) {
-                    req.logout();
-                    res.send('not allowed', 403)
-                }
-                else {
-                    db.sadd('users', uid);
-                    db.set('user:' + uid, userdata, function(err, data) {});
-                    res.send('new user, first login', 200);
-                }
-            });
+  var uid = req.getAuthDetails().user.user_id;
+  var userdata = JSON.stringify(req.getAuthDetails());
+  //console.dir(req.getAuthDetails());
+  db.sismember('users', uid, function(err, registered) {
+    if (registered) {
+      db.set('user:' + uid, userdata, function(err, data) {});
+      res.send('OK', 200);
+    }
+    else {
+      db.sismember('invitees', req.getAuthDetails().user.username, function(err, invited) {
+        if (invited) {
+          db.sadd('users', uid);
+          db.set('user:' + uid, userdata, function(err, data) {});
+          res.send('new user, first login', 200);
         }
-
         else {
-            db.set('user:' + uid, userdata, function(err, data) {});
-            res.send('OK', 200);
+          req.logout();
+          res.send('not allowed', 403)
         }
-    });
+      });
+    }
+  });
 });
 
 app.get('/case/:id/:page/edit', function(req, res) {
-    if (req.isAuthenticated()) {
-        db.hgetall('case:' + req.params.id + ':page:' + req.params.page, function(err, data) {
-            console.dir(data);
-            if (!data)return res.send('NOT FOUND', 404);
-            db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id,
-                function(err, editor) {
-                    if (editor) {
-                        res.render('edit', {
-                            title: 'edit',
-                            caseid: req.params.id,
-                            page: req.params.page,
-                            signed_in: req.isAuthenticated(),
-                            user: req.getAuthDetails().user.username
-                        });
-                    }
-                    else res.send('NOT ALLOWED', 403);
-                });
-        });
-    }
-    else res.redirect('/');
+  if (!req.isAuthenticated()) res.redirect('/');
+  else {
+    db.hgetall('case:' + req.params.id + ':page:' + req.params.page, function(err, data) {
+      //console.dir(data);
+      if (!data) return res.send('NOT FOUND', 404);
+      db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id, function(err, editor) {
+        if (!editor) res.send('NOT ALLOWED', 403);
+        else {
+          res.render('edit', {
+            title: 'edit',
+            caseid: req.params.id,
+            page: req.params.page,
+            signed_in: req.isAuthenticated(),
+            user: req.getAuthDetails().user.username
+          });
+        }
+      });
+    });
+  }
 });
 
 app.post('/case/:id/newpage', function(req, res) {
-    console.log('newpage triggered');
-    db.sismember('case:' + req.params.id + ':users',
-        req.getAuthDetails().user.user_id,
-        function(err, editor) {
-            if (editor) {
-                var page = 2;
-                var cid = req.params.id;
-                var pagedata = req.body;
-                pagedata.texts = 'Double click to add text';
-                pagedata.creator = req.getAuthDetails().user.username;
-                pagedata.cid = req.params.id;
-                requestHandlers.newpage(req, res, cid, page, db, pagedata);
-            }
-            else res.send('FORBIDDEN', 403);
-        });
+  console.log('newpage triggered');
+  db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id, function(err, editor) {
+    if (!editor) res.send('FORBIDDEN', 403);
+    else {
+      var page = 2;
+      var cid = req.params.id;
+      var pagedata = req.body;
+      pagedata.texts = 'Double click to add text';
+      pagedata.creator = req.getAuthDetails().user.username;
+      pagedata.cid = req.params.id;
+      requestHandlers.newpage(req, res, cid, page, db, pagedata);
+    }
+  });
 });
 
 app.put('/case/:id/:page', function(req, res) {
-    db.sismember('case:' + req.params.id + ':users',
-        req.getAuthDetails().user.user_id,
-        function(err, editor) {
-            if (editor) {
-                var data = req.body;
-                data.cid = req.params.id;
-                data.creator = req.getAuthDetails().user.username;
-                db.hmset('case:' + req.params.id + ':page:' + req.params.page, data);
-                console.log('saved page');
-                res.send('OK', 200)
-            }
-            else res.send('FORBIDDEN', 403);
-        });
+  db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id, function(err, editor) {
+    if (!editor) res.send('FORBIDDEN', 403);
+    else {
+      var data = req.body;
+      data.cid = req.params.id;
+      data.creator = req.getAuthDetails().user.username;
+      db.hmset('case:' + req.params.id + ':page:' + req.params.page, data);
+      console.log('saved page');
+      res.send('OK', 200)
+    }
+  });
 });
 
 app.post('/case/:id/:page/delete', function(req, res) {
-    console.dir('delete page triggered');
-    db.sismember('case:' + req.params.id + ':users',
-        req.getAuthDetails().user.user_id,
-        function(err, editor) {
-            if (editor) {
-                db.del('case:' + req.params.id + ':page:' + req.params.page);
-                res.send('OK', 200);
-            }
-            else res.send('FORBIDDEN', 403)
-        })
+  console.dir('delete page triggered');
+  db.sismember('case:' + req.params.id + ':users', req.getAuthDetails().user.user_id, function(err, editor) {
+    if (!editor) res.send('FORBIDDEN', 403)
+    else {
+      db.del('case:' + req.params.id + ':page:' + req.params.page);
+      res.send('OK', 200);
+    }
+  })
 });
 
 app.get('/sign_out', function(req, res, params) {
-    req.logout();
-    res.send('<button id="twitbutt">Sign in with twitter</button>');
+  req.logout();
+  res.send('<button id="twitbutt">Sign in with twitter</button>');
 });
 
 
 app.get('/readme', function(req, res) {
-    res.render('readme', {
-        title: 'README',
-        signed_in: req.isAuthenticated(),
-        user: username(req, res)
-    });
+  res.render('readme', {
+    title: 'README',
+    signed_in: req.isAuthenticated(),
+    user: username(req, res)
+  });
 });
 
 app.get('/colophon', function(req, res) {
-    res.render('colophon', {
-        title: 'Colophon',
-        signed_in: req.isAuthenticated(),
-        user: username(req, res)
-    });
+  res.render('colophon', {
+    title: 'Colophon',
+    signed_in: req.isAuthenticated(),
+    user: username(req, res)
+  });
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', {
-        title: 'About',
-        signed_in: req.isAuthenticated(),
-        user: username(req, res)
-    });
+  res.render('about', {
+    title: 'About',
+    signed_in: req.isAuthenticated(),
+    user: username(req, res)
+  });
 });
 
 app.get('/img/:img', function(req, res) {
-    if (req.isAuthenticated()) {
-        var image = __dirname + '/img/' + req.params.img;
-        fs.readFile(image, 'binary', function(error, file) {
-            if (error) return res.send('huh?', 404);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'image/jpeg');
-            res.write(file, 'binary');
-            res.end();
-        });
-    }
-    else res.redirect('/')
+  if (!req.isAuthenticated()) res.redirect('/');
+  else {
+    var image = __dirname + '/img/' + req.params.img;
+    fs.readFile(image, 'binary', function(error, file) {
+      if (error) return res.send('huh?', 404);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'image/jpeg');
+      res.write(file, 'binary');
+      res.end();
+    });
+  }
 });
 
 app.post('/case/:id/feedback', function(req, res) {
-    feedback = req.body;
-    console.dir(feedback);
-    res.send('OK', 200);
+  feedback = req.body;
+  console.dir(feedback);
+  res.send('OK', 200);
 });
 
 app.post('/image/:id/:page', function(req, res) {
-    console.log('POST /image/ called');
-    if (req.isAuthenticated()) requestHandlers.postImage2(req, res, db);
-    else res.send('not logged in', 200)
+  console.log('POST /image/ called');
+  if (!req.isAuthenticated()) res.send('not logged in', 200);
+  else requestHandlers.postImage2(req, res, db);
 });
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-    //console.dir(process.env);
-    console.log('Express server listening on port %d in %s mode', app.address().port, app.settings.env);
+  //console.dir(process.env);
+  console.log('Express server listening on port %d in %s mode', app.address().port, app.settings.env);
 });
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,140 +1,86 @@
-var formidable = require('formidable'),
-    url = require('url');
+var formidable = require('formidable');
+var url = require('url');
 
-function getCases(req, res, data, i, sendcases, db){
-    if(data[i]){
-        db.hgetall('case:' + data[i] + ':page:1', function(err, sendcase){
-            sendcases[i] = sendcase;
-            i++;
-            if(data[i]){getCases(data, i, sendcases)}
-            else{console.log('rendering cases');res.render('cases', {
-                title: 'Cases',
-                signed_in: req.isAuthenticated(),
-                user: req.getAuthDetails().user.username,
-                cases: sendcases
-            })}
+function rendercase(req, res, theCase, editor, db) {
+  db.get('markdown-help', function(err, data) {
+    mdhelp = JSON.parse(data);
+    db.lrange('case:' + req.params.id + ':page:' + req.params.page + ":radios", 0, -1, function(err, radioIDs) {
+      theCase.radios = [];
+      radioIDs.forEach(function(radioID, ID) {
+        theCase.radios[ID] = [];
+        theCase.radios[ID].ID = radioID;
+        db.lrange("radio:" + radioID, 0, -1, function(err, images) {
+          theCase.radios[ID].images = [];
+          images.forEach(function(image, imgID) {
+            theCase.radios[ID].images[imgID] = image;
+          });
+          if (!radioIDs[ID + 1]) {
+            console.log('last radio');
+            console.dir(theCase);
+            return res.render('case', {
+              title: theCase.title || ' - untitled',
+              radios: theCase.radios || '',
+              texts: [theCase.texts] || '',
+              creator: theCase.creator || '',
+              mdhelp: mdhelp,
+              signed_in: req.isAuthenticated(),
+              user: req.getAuthDetails().user.username,
+              cid: req.params.id,
+              prevpage: parseInt(req.params.page, 10) - 1,
+              nextpage: parseInt(req.params.page, 10) + 1,
+              page: req.params.page,
+              editor: editor,
+              meta_private: theCase.meta_private || 0
+            });
+          }
         });
-    } else{return 'No data'}
-}
-
-function popradios(req, res, theCase, db){
-    db.lrange('case:' + req.params.id + ':page:' + req.params.page + ":radios",
-        0, -1, function(err, radioIDs){
-            theCase.radios = [];
-            radioIDs.forEach(function(radioID, ID){
-                theCase.radios[ID] = [];
-                theCase.radios[ID].ID = radioID;
-                db.lrange("radio:" + radioID, 0, -1, function(err, images){
-                    theCase.radios[ID].images = [];
-                    images.forEach(function(image, imgID){
-                        theCase.radios[ID].images[imgID] = image;
-                    });
-                    if(!radioIDs[ID+1])
-                    {
-                        console.log('last radio');
-                        return res.render('case', {
-                            title: theCase.title || ' - untitled',
-                            radios: theCase.radios || '',
-                            texts: [theCase.texts] || '',
-                            creator: theCase.creator || '',
-                            mdhelp: mdhelp,
-                            signed_in: req.isAuthenticated(),
-                            user: req.getAuthDetails().user.username,
-                            cid: req.params.id,
-                            prevpage: parseInt(req.params.page, 10) - 1,
-                            nextpage: parseInt(req.params.page, 10) + 1,
-                            page: req.params.page,
-                            editor: editor,
-                            meta_private: theCase.meta_private || 0
-                        });
-                    }
-                    //console.dir(theCase.radios[ID].images);
-                });
-            });
+      });
     });
+  });
 }
 
-function rendercase(req, res, theCase, editor, db){
-    db.get("markdown-help", function(err, data){
-        mdhelp = JSON.parse(data);
-        db.lrange('case:' + req.params.id + ':page:' + req.params.page + ":radios",
-            0, -1, function(err, radioIDs){
-                theCase.radios = [];
-                radioIDs.forEach(function(radioID, ID){
-                    theCase.radios[ID] = [];
-                    theCase.radios[ID].ID = radioID;
-                    db.lrange("radio:" + radioID, 0, -1, function(err, images){
-                        theCase.radios[ID].images = [];
-                        images.forEach(function(image, imgID){
-                            theCase.radios[ID].images[imgID] = image;
-                        });
-                        if(!radioIDs[ID+1]){
-                            console.log('last radio');
-                            console.dir(theCase);
-                            return res.render('case', {
-                                title: theCase.title || ' - untitled',
-                                radios: theCase.radios || '',
-                                texts: [theCase.texts] || '',
-                                creator: theCase.creator || '',
-                                mdhelp: mdhelp,
-                                signed_in: req.isAuthenticated(),
-                                user: req.getAuthDetails().user.username,
-                                cid: req.params.id,
-                                prevpage: parseInt(req.params.page, 10) - 1,
-                                nextpage: parseInt(req.params.page, 10) + 1,
-                                page: req.params.page,
-                                editor: editor,
-                                meta_private: theCase.meta_private || 0
-                            });
-                        }
-                    });
-                });
-            });
-});
-}
-
-function postImage2(req, res, db){
+function postImage2(req, res, db) {
   var day = new Date();
   var timems = day.getTime().toString();
   var iteration = 0;
   var form = new formidable.IncomingForm(),
-      files = [],
-      fields = [];
+          files = [],
+          fields = [];
   form
-    .on('field', function(field, value) {
-        fields.push([field, value]);
-    })
-    .on('fileBegin', function(field, file) {
-          if(file.type='image/jpeg') {
-            file.path = __dirname + '/img/' + timems + '.' + iteration + '.jpg';
-            db.rpush('radio:' + timems, '/img/' + timems + '.' + iteration + '.jpg');
-            iteration ++;
-          }
-          files.push([field, file]);
-    })
-    .on('end', function() {
-        console.log('-> upload done');
-        db.sadd('image:' + timems, req.params.id);
-        db.rpush('case:' + req.params.id + ':page:' + req.params.page + ':radios', timems);
-        res.send(timems + '|' + iteration, 200);
-    });
-  form.parse(req, function(err, fields, files){
+          .on('field', function(field, value) {
+    fields.push([field, value]);
+  })
+          .on('fileBegin', function(field, file) {
+            if (file.type = 'image/jpeg') {
+              file.path = __dirname + '/img/' + timems + '.' + iteration + '.jpg';
+              db.rpush('radio:' + timems, '/img/' + timems + '.' + iteration + '.jpg');
+              iteration ++;
+            }
+            files.push([field, file]);
+          })
+          .on('end', function() {
+            console.log('-> upload done');
+            db.sadd('image:' + timems, req.params.id);
+            db.rpush('case:' + req.params.id + ':page:' + req.params.page + ':radios', timems);
+            res.send(timems + '|' + iteration, 200);
+          });
+  form.parse(req, function(err, fields, files) {
   });
 }
 
-function newpage(req, res, cid, page, db, pagedata){
-            var trypage = 'case:' + cid + ':page:' + page;
-            db.get(trypage, function(err, data) {
-                if (data) {
-                    page++;
-                    newpage(req, res, cid, page, db, pagedata);
-                } 
-                else {
-                    db.set(trypage, JSON.stringify(pagedata));
-                    res.send('/case/' + cid + '/' + page, 200);
-                }
-            });
-        }
+function newpage(req, res, cid, page, db, pagedata) {
+  var trypage = 'case:' + cid + ':page:' + page;
+  db.get(trypage, function(err, data) {
+    if (data) {
+      page++;
+      newpage(req, res, cid, page, db, pagedata);
+    }
+    else {
+      db.set(trypage, JSON.stringify(pagedata));
+      res.send('/case/' + cid + '/' + page, 200);
+    }
+  });
+}
 
 exports.rendercase = rendercase;
 exports.getCases = getCases;
